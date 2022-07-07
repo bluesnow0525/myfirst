6eb40049446730df00a11ea1f97447554cb6cfe0
bluesnow0525 <108323356+bluesnow0525@users.noreply.github.com>
2022-07-07 15:09:11 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
-from matplotlib.pyplot import draw
+
 import pygame
 import os
+import random 
 
 FPS=60
 WIDTH=600
 HEIGHT=600
-ground_color=(230,230,250)
+ground_color=(250,250,250)
 player1_color=(255,69,0)
 player2_color=(0,191,255)
 bullet_color=(255,215,0)
@@ -21,6 +22,20 @@ player1_img = pygame.image.load(os.path.join("img", "player1.jpg")).convert()
 player2_img = pygame.image.load(os.path.join("img", "player2.jpg")).convert()
 ground_img = pygame.image.load(os.path.join("img", "ground.jpg")).convert()
 background_img = pygame.image.load(os.path.join("img", "background.png")).convert()
+fire_img = pygame.image.load(os.path.join("img", "fire.jpg")).convert()
+rock_imgs = []
+for i in range(6):
+    rock_imgs.append(pygame.image.load(os.path.join("img", f"rock{i}.png")).convert())
+#music
+shoot_sound = pygame.mixer.Sound(os.path.join("sound", "shoot.wav"))
+die_sound = pygame.mixer.Sound(os.path.join("sound", "rumble.ogg"))
+expl_sounds = [
+    pygame.mixer.Sound(os.path.join("sound", "expl0.wav")),
+    pygame.mixer.Sound(os.path.join("sound", "expl1.wav"))
+]
+pygame.mixer.music.load(os.path.join("sound", "background.ogg"))
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1)
 #sprite 物件
 class Player1(pygame.sprite.Sprite):
     def __init__(self):
@@ -31,8 +46,8 @@ class Player1(pygame.sprite.Sprite):
         self.rect.centerx=WIDTH*3/4
         self.rect.bottom=HEIGHT-51
         self.ori_botton=0
-        self.speed_x=4
-        self.speed_y=2
+        self.speed_x=6
+        self.speed_y=3
         self.speed_jumpup=10
         self.ifjump=0
         self.ifjumpup=0
@@ -52,7 +67,7 @@ class Player1(pygame.sprite.Sprite):
             self.rect.y+=self.speed_y
             ifhit=pygame.sprite.spritecollide(player1,grounds,False)
             if(self.rect.bottom>=HEIGHT-50 or ifhit):
-                self.rect.y-=2
+                self.rect.y-=3
                 self.ifjump=0
         if(key_press[pygame.K_RIGHT]):
             if(self.way==1):
@@ -61,9 +76,11 @@ class Player1(pygame.sprite.Sprite):
             self.rect.x+=self.speed_x
             ifhit=pygame.sprite.spritecollide(player1,grounds,False)
             if ifhit:
-                self.rect.x-=5
-                self.rect.y-=2
-            if(self.rect.bottom<=HEIGHT-200 and self.rect.left>=325):
+                self.rect.x-=7
+                self.rect.y-=3
+            if(self.rect.bottom<=HEIGHT-200 and self.rect.left>=405):
+                self.ifjump=1
+            if(self.rect.bottom<=250 and self.rect.left>=100):
                 self.ifjump=1
         if(key_press[pygame.K_LEFT]):
             if(self.way==2):
@@ -72,9 +89,11 @@ class Player1(pygame.sprite.Sprite):
             self.rect.x-=self.speed_x
             ifhit=pygame.sprite.spritecollide(player1,grounds,False)
             if ifhit:
-                self.rect.x+=5
-                self.rect.y-=2
+                self.rect.x+=7
+                self.rect.y-=3
             if(self.rect.bottom<=HEIGHT-200 and self.rect.right<=175):
+                self.ifjump=1
+            if(self.rect.bottom<=250 and self.rect.right<=500):
                 self.ifjump=1
         if self.rect.right>WIDTH:
             self.rect.right=WIDTH
@@ -88,6 +107,7 @@ class Player1(pygame.sprite.Sprite):
         bullet=Bullet(self.rect.center,self.way)
         all_sprites.add(bullet)
         p1bullets.add(bullet)
+        shoot_sound.play()
     def rotate(self):
         self.image = pygame.transform.flip(self.image,True,False)
         center = self.rect.center
@@ -102,8 +122,8 @@ class Player2(pygame.sprite.Sprite):
         self.rect.centerx=WIDTH/4
         self.rect.bottom=HEIGHT-50
         self.ori_botton=0
-        self.speed_x=4
-        self.speed_y=2
+        self.speed_x=6
+        self.speed_y=3
         self.speed_jumpup=10
         self.ifjump=0
         self.ifjumpup=0
@@ -123,7 +143,7 @@ class Player2(pygame.sprite.Sprite):
             self.rect.y+=self.speed_y
             ifhit=pygame.sprite.spritecollide(player2,grounds,False)
             if(self.rect.bottom>=HEIGHT-50 or ifhit):
-                self.rect.y-=2
+                self.rect.y-=3
                 self.ifjump=0
         if(key_press[pygame.K_d]):
             if(self.way==1):
@@ -132,9 +152,11 @@ class Player2(pygame.sprite.Sprite):
             self.rect.x+=self.speed_x
             ifhit=pygame.sprite.spritecollide(player2,grounds,False)
             if ifhit:
-                self.rect.x-=5
-                self.rect.y-=2
-            if(self.rect.bottom<=HEIGHT-200 and self.rect.left>=325):
+                self.rect.x-=7
+                self.rect.y-=3
+            if(self.rect.bottom<=HEIGHT-200 and self.rect.left>=405):
+                self.ifjump=1
+            if(self.rect.bottom<=250 and self.rect.left>=100):
                 self.ifjump=1
         if(key_press[pygame.K_a]):
             if(self.way==2):
@@ -143,9 +165,11 @@ class Player2(pygame.sprite.Sprite):
             self.rect.x-=self.speed_x
             ifhit=pygame.sprite.spritecollide(player2,grounds,False)
             if ifhit:
-                self.rect.x+=5
-                self.rect.y-=2
+                self.rect.x+=7
+                self.rect.y-=3
             if(self.rect.bottom<=HEIGHT-200 and self.rect.right<=175):
+                self.ifjump=1
+            if(self.rect.bottom<=250 and self.rect.right<=500):
                 self.ifjump=1
         if self.rect.right>WIDTH:
             self.rect.right=WIDTH
@@ -159,6 +183,7 @@ class Player2(pygame.sprite.Sprite):
         bullet=Bullet(self.rect.center,self.way)
         all_sprites.add(bullet)
         p2bullets.add(bullet)
+        shoot_sound.play()
     def rotate(self):
         self.image = pygame.transform.flip(self.image,True,False)
         center = self.rect.center
@@ -187,17 +212,75 @@ class Ground(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_ori = random.choice(rock_imgs) 
+        self.image_ori.set_colorkey((0,0,0))
+        self.image = self.image_ori.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * 0.85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-180, -100)
+        self.speedy = random.randrange(2, 5)
+        self.speedx = random.randrange(-3, 3)
+        self.total_degree = 0
+        self.rot_degree = random.randrange(-3, 3)
+
+    def rotate(self):
+        self.total_degree += self.rot_degree
+        self.total_degree = self.total_degree % 360
+        self.image = pygame.transform.rotate(self.image_ori, self.total_degree)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+
+    def update(self):
+        self.rotate()
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(2, 10)
+            self.speedx = random.randrange(-3, 3)
+class Fire(pygame.sprite.Sprite):
+    def __init__(self,x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(fire_img, (200, 30))
+        self.image.set_colorkey((255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = HEIGHT-70
+
 all_sprites=pygame.sprite.Group()
 p1bullets=pygame.sprite.Group()
 p2bullets=pygame.sprite.Group()
 grounds=pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+fires=pygame.sprite.Group()
 g1=Ground(0,HEIGHT-50,WIDTH,50)
 grounds.add(g1)
 all_sprites.add(g1)
 g2=Ground(175,HEIGHT-200,250,30)
 grounds.add(g2)
 all_sprites.add(g2)
+g3=Ground(0,250,100,30)
+grounds.add(g3)
+all_sprites.add(g3)
+g4=Ground(500,250,100,30)
+grounds.add(g4)
+all_sprites.add(g4)
+fire1=Fire(0)
+all_sprites.add(fire1)
+fires.add(fire1)
+fire2=Fire(200)
+all_sprites.add(fire2)
+fires.add(fire2)
+fire3=Fire(400)
+all_sprites.add(fire3)
+fires.add(fire3)
 player1=Player1()
 player2=Player2()
 p1=pygame.sprite.Group()
@@ -206,6 +289,7 @@ p1.add(player1)
 p2.add(player2)
 all_sprites.add(player1)
 all_sprites.add(player2)
+
 font_name = os.path.join("font.ttf")
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
@@ -214,9 +298,15 @@ def draw_text(surf, text, size, x, y):
     text_rect.centerx = x
     text_rect.top = y
     surf.blit(text_surface, text_rect)
+def new_rock():
+    r = Rock()
+    all_sprites.add(r)
+    rocks.add(r)
 #screens
 def draw_init():
-    draw_text(screen, "遊戲開始", 64, WIDTH/2, HEIGHT/4)
+    draw_text(screen, "按任意鍵開始", 64, WIDTH/2, HEIGHT/4)
+    draw_text(screen, "人類:上、左、右、右Ctrl鍵", 22, WIDTH/2, HEIGHT/2)
+    draw_text(screen, "企鵝:W、A、D、空白鍵", 22, WIDTH/2, HEIGHT*3/4)
     pygame.display.update()
     waiting = True
     while waiting:
@@ -231,7 +321,8 @@ def draw_init():
                 return False
 def drawp1win():
     screen.fill((0,0,0))
-    draw_text(screen,"人類 win",64,WIDTH/2,HEIGHT/2)
+    draw_text(screen,"人類 win",64,WIDTH/2,HEIGHT/4)
+    draw_text(screen, "按m後重新開始", 22, WIDTH/2, HEIGHT/2)
     pygame.display.update()
     waiting = True
     while waiting:
@@ -240,10 +331,15 @@ def drawp1win():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                return True   
+                return True 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:                   
+                    waiting = False                    
+                    return False  
 def drawp2win():
     screen.fill((0,0,0))
-    draw_text(screen,"企鵝 win",64,WIDTH/2,HEIGHT/2)
+    draw_text(screen,"企鵝 win",64,WIDTH/2,HEIGHT/4)
+    draw_text(screen, "按m後重新開始", 22, WIDTH/2, HEIGHT/2)
     pygame.display.update()
     waiting = True
     while waiting:
@@ -253,6 +349,10 @@ def drawp2win():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:
+                    waiting = False
+                    return False
 def draw_life(surf,hp,x,y):
     if hp<0:
         hp=0
@@ -274,10 +374,34 @@ while running:
         if close:
             break
         show_init = False
+        for i in range(5):
+            new_rock()
     if show_p1win:
-        drawp1win()
+        close = drawp1win()
+        if close:
+            break
+        player1=Player1()
+        player2=Player2()
+        p1=pygame.sprite.Group()
+        p2=pygame.sprite.Group()
+        p1.add(player1)
+        p2.add(player2)
+        all_sprites.add(player1)
+        all_sprites.add(player2)
+        show_p1win=False
     if show_p2win:
-        drawp2win()
+        close = drawp2win()
+        if close:
+            break
+        player1=Player1()
+        player2=Player2()
+        p1=pygame.sprite.Group()
+        p2=pygame.sprite.Group()
+        p1.add(player1)
+        p2.add(player2)
+        all_sprites.add(player1)
+        all_sprites.add(player2)
+        show_p2win=False
     clock.tick(FPS)
     #get
     for event in pygame.event.get():
@@ -290,7 +414,7 @@ while running:
             if event.key == pygame.K_w:
                 if(player2.ifjump==0 and player2.ifjumpup==0):
                     player2.jump()
-            if event.key == pygame.K_p:
+            if event.key == pygame.K_RCTRL:
                 player1.shoot()
             if event.key == pygame.K_SPACE:
                 player2.shoot() 
@@ -299,28 +423,50 @@ while running:
     all_sprites.update()
     hitp1=pygame.sprite.spritecollide(player1,p2bullets,True)
     hitp2=pygame.sprite.spritecollide(player2,p1bullets,True)
+    fhitp1=pygame.sprite.spritecollide(player1,fires,False)
+    fhitp2=pygame.sprite.spritecollide(player2,fires,False)
     pygame.sprite.groupcollide(grounds,p1bullets,False,True)
     pygame.sprite.groupcollide(grounds,p2bullets,False,True)
+    pygame.sprite.groupcollide(rocks,p1bullets,False,True)
+    pygame.sprite.groupcollide(rocks,p2bullets,False,True)
     hits=pygame.sprite.groupcollide(p1,p2,False,False)  
+    rhit=pygame.sprite.groupcollide(rocks,grounds,True,False)
+    rhitp1=pygame.sprite.spritecollide(player1,rocks,True)
+    rhitp2=pygame.sprite.spritecollide(player2,rocks,True)
+    if rhit:
+        new_rock()
     if hits:
         if(player1.rect.x>player2.rect.x):
             player1.rect.x+=20
             player2.rect.x-=20
         else:
             player1.rect.x-=20
-            player2.rect.x+=20
-        
+            player2.rect.x+=20  
     if hitp1:
-        player1.life-=10
+        random.choice(expl_sounds).play()
+        player1.life-=5
     if hitp2:
-        player2.life-=10
+        random.choice(expl_sounds).play()
+        player2.life-=5
+    for hit in rhitp1:
+        new_rock()
+        player1.life-=hit.radius * 2
+    for hit in rhitp2:
+        new_rock()
+        player2.life-=hit.radius * 2
+    if fhitp1:
+        player1.life-=0.1
+    if fhitp2:
+        player2.life-=0.1
     if(player1.life<=0):
         player1.kill()
         player2.kill()
+        die_sound.play()
         show_p2win=True
     if(player2.life<=0):
         player1.kill()
         player2.kill()
+        die_sound.play()
         show_p1win=True
     
     #screen show
